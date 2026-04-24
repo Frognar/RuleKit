9619f08c2233ff1683fbd6688565f5b191299bbd
frognar <sebastian.przyszlak@gmail.com>
2026-04-24 10:52:47 +0200

    refactor: replace alwaysPassingRule and alwaysFailingRule with predicates in RuleTests

--- tests/RuleKit.Tests/RuleTests.cs
+++ tests/RuleKit.Tests/RuleTests.cs
@@ -2,8 +2,8 @@ namespace RuleKit.Tests;
 
 public class RuleTests
 {
-    private readonly Rule<int> alwaysPassingRule = CreateRule<int>(_ => true);
-    private readonly Rule<int> alwaysFailingRule = CreateRule<int>(_ => false);
+    private readonly Func<int, bool> alwaysTruePredicate = _ => true;
+    private readonly Func<int, bool> alwaysFalsePredicate = _ => false;
     private static Rule<T> CreateRule<T>(Func<T, bool> predicate, string code = "general", string message = "failed") =>
         Rule.FromPredicate(predicate, code, message);
 
@@ -20,7 +20,7 @@ public class RuleTests
     [Fact]
     public void FromPredicate_ShouldThrowArgumentNullException_WhenCodeIsNull()
     {
-        var exception = Assert.Throws<ArgumentNullException>(() => CreateRule<int>(_ => true, code: null!));
+        var exception = Assert.Throws<ArgumentNullException>(() => CreateRule(alwaysTruePredicate, code: null!));
         Assert.Equal("code", exception.ParamName);
     }
 
@@ -29,14 +29,14 @@ public class RuleTests
     [InlineData(" ")]
     public void FromPredicate_ShouldThrowArgumentException_WhenCodeIsEmpty(string code)
     {
-        var exception = Assert.Throws<ArgumentException>(() => CreateRule<int>(_ => true, code: code));
+        var exception = Assert.Throws<ArgumentException>(() => CreateRule(alwaysTruePredicate, code: code));
         Assert.Equal("code", exception.ParamName);
     }
 
     [Fact]
     public void FromPredicate_ShouldThrowArgumentNullException_WhenMessageIsNull()
     {
-        var exception = Assert.Throws<ArgumentNullException>(() => CreateRule<int>(_ => true, message: null!));
+        var exception = Assert.Throws<ArgumentNullException>(() => CreateRule(alwaysTruePredicate, message: null!));
         Assert.Equal("message", exception.ParamName);
     }
 
@@ -45,15 +45,18 @@ public class RuleTests
     [InlineData(" ")]
     public void FromPredicate_ShouldThrowArgumentException_WhenMessageIsEmpty(string message)
     {
-        var exception = Assert.Throws<ArgumentException>(() => CreateRule<int>(_ => true, message: message));
+        var exception = Assert.Throws<ArgumentException>(() => CreateRule(alwaysTruePredicate, message: message));
         Assert.Equal("message", exception.ParamName);
     }
 
     [Fact]
     public void FromPredicate_ShouldReturnPassedResult_WhenPredicateIsTrue()
     {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+
         // act
-        var result = alwaysPassingRule(0);
+        var result = rule(0);
 
         // assert
         Assert.IsType<RulePassed>(result);
@@ -62,79 +65,8 @@ public class RuleTests
     [Fact]
     public void FromPredicate_ShouldReturnFailedResult_WhenPredicateIsFalse()
     {
-        // act
-        var result = alwaysFailingRule(0);
-
-        // assert
-        Assert.IsType<RuleFailed>(result);
-    }
-
-    [Fact]
-    public void FromPredicate_ShouldReturnFailedResultWithCode_WhenPredicateIsFalse()
-    {
-        // act
-        var result = alwaysFailingRule(0);
-
-        // assert
-        var failed = Assert.IsType<RuleFailed>(result);
-        Assert.Equal("general", failed.Code);
-    }
-
-    [Fact]
-    public void FromPredicate_ShouldReturnFailedResultWithMessage_WhenPredicateIsFalse()
-    {
-        // act
-        var result = alwaysFailingRule(0);
-
-        // assert
-        var failed = Assert.IsType<RuleFailed>(result);
-        Assert.Equal("failed", failed.Message);
-    }
-
-    [Fact]
-    public void Not_ShouldThrowArgumentNullException_WhenRuleIsNull()
-    {
-        var exception = Assert.Throws<ArgumentNullException>(() => Negate<int>(null!));
-        Assert.Equal("rule", exception.ParamName);
-    }
-
-    [Fact]
-    public void Not_ShouldThrowArgumentNullException_WhenCodeIsNull()
-    {
-        var exception = Assert.Throws<ArgumentNullException>(() => Negate(alwaysPassingRule, code: null!));
-        Assert.Equal("code", exception.ParamName);
-    }
-
-    [Theory]
-    [InlineData("")]
-    [InlineData(" ")]
-    public void Not_ShouldThrowArgumentException_WhenCodeIsEmpty(string code)
-    {
-        var exception = Assert.Throws<ArgumentException>(() => Negate(alwaysPassingRule, code: code));
-        Assert.Equal("code", exception.ParamName);
-    }
-
-    [Fact]
-    public void Not_ShouldThrowArgumentNullException_WhenMessageIsNull()
-    {
-        var exception = Assert.Throws<ArgumentNullException>(() => Negate(alwaysPassingRule, message: null!));
-        Assert.Equal("message", exception.ParamName);
-    }
-
-    [Theory]
-    [InlineData("")]
-    [InlineData(" ")]
-    public void Not_ShouldThrowArgumentException_WhenMessageIsEmpty(string message)
-    {
-        var exception = Assert.Throws<ArgumentException>(() => Negate(alwaysPassingRule, message: message));
-        Assert.Equal("message", exception.ParamName);
-    }
-
-    [Fact]
-    public void Not_ShouldReturnFailedResult_WhenInnerRuleReturnsPassedResult()
-    {
         // arrange
-        var rule = Negate(alwaysPassingRule);
+        var rule = CreateRule(alwaysFalsePredicate);
 
         // act
         var result = rule(0);
@@ -144,10 +76,149 @@ public class RuleTests
     }
 
     [Fact]
+    public void FromPredicate_ShouldReturnFailedResultWithCode_WhenPredicateIsFalse()
+    {
+        // arrange
+        var rule = CreateRule(alwaysFalsePredicate, code: "always-false");
+
+        // act
+        var result = rule(0);
+
+        // assert
+        var failed = Assert.IsType<RuleFailed>(result);
+        Assert.Equal("always-false", failed.Code);
+    }
+
+    [Fact]
+    public void FromPredicate_ShouldReturnFailedResultWithMessage_WhenPredicateIsFalse()
+    {
+        // arrange
+        var rule = CreateRule(alwaysFalsePredicate, message: "always-false");
+
+        // act
+        var result = rule(0);
+
+        // assert
+        var failed = Assert.IsType<RuleFailed>(result);
+        Assert.Equal("always-false", failed.Message);
+    }
+
+    [Fact]
+    public void Not_ShouldThrowArgumentNullException_WhenRuleIsNull()
+    {
+        // act
+        var exception = Assert.Throws<ArgumentNullException>(() => Negate<int>(null!));
+
+        // assert
+        Assert.Equal("rule", exception.ParamName);
+    }
+
+    [Fact]
+    public void Not_ShouldThrowArgumentNullException_WhenCodeIsNull()
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+        
+        // act
+        var exception = Assert.Throws<ArgumentNullException>(() => Negate(rule, code: null!));
+
+        // assert
+        Assert.Equal("code", exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Not_ShouldThrowArgumentException_WhenCodeIsEmpty(string code)
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+
+        // act
+        var exception = Assert.Throws<ArgumentException>(() => Negate(rule, code: code));
+
+        // assert
+        Assert.Equal("code", exception.ParamName);
+    }
+
+    [Fact]
+    public void Not_ShouldThrowArgumentNullException_WhenMessageIsNull()
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+
+        // act
+        var exception = Assert.Throws<ArgumentNullException>(() => Negate(rule, message: null!));
+
+        // assert
+        Assert.Equal("message", exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Not_ShouldThrowArgumentException_WhenMessageIsEmpty(string message)
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+
+        // act
+        var exception = Assert.Throws<ArgumentException>(() => Negate(rule, message: message));
+
+        // assert
+        Assert.Equal("message", exception.ParamName);
+    }
+
+    [Fact]
+    public void Not_ShouldReturnFailedResult_WhenInnerRuleReturnsPassedResult()
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+        rule = Negate(rule);
+
+        // act
+        var result = rule(0);
+
+        // assert
+        Assert.IsType<RuleFailed>(result);
+    }
+
+    [Fact]
+    public void Not_ShouldReturnFailedResultWithCode_WhenInnerRuleReturnsPassedResult()
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+        rule = Negate(rule, code: "negation");
+
+        // act
+        var result = rule(0);
+
+        // assert
+        var failed = Assert.IsType<RuleFailed>(result);
+        Assert.Equal("negation", failed.Code);
+    }
+
+    [Fact]
+    public void Not_ShouldReturnFailedResultWithMessage_WhenInnerRuleReturnsPassedResult()
+    {
+        // arrange
+        var rule = CreateRule(alwaysTruePredicate);
+        rule = Negate(rule, message: "negation");
+
+        // act
+        var result = rule(0);
+
+        // assert
+        var failed = Assert.IsType<RuleFailed>(result);
+        Assert.Equal("negation", failed.Message);
+    }
+
+    [Fact]
     public void Not_ShouldReturnPassedResult_WhenInnerRuleReturnsFailedResult()
     {
         // arrange
-        var rule = Negate(alwaysFailingRule);
+        var rule = CreateRule(alwaysFalsePredicate);
+        rule = Negate(rule);
 
         // act
         var result = rule(0);
